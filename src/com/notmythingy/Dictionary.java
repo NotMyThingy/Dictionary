85afa79b1e08a6013b0598bb996d00d8d65449fc
NotMyThingy <miska.janhunen@protonmail.com>
2020-10-27 21:22:39 +0200

    Check if word has already been drawn, or all words has been drawn (exit application).

--- src/com/notmythingy/Dictionary.java
+++ src/com/notmythingy/Dictionary.java
@@ -4,29 +4,39 @@ import java.util.*;
 
 public class Dictionary {
 
-    private Map<String, String> translations;
+    private final Map<String, String> translations;
+    private final List<String> usedWords;
+    private final PracticeApplication practiceApplication;
 
-    public Dictionary() {
-        translations = new HashMap<>();
+    public Dictionary(PracticeApplication practiceApplication) {
+        this.translations = new HashMap<>();
+        this.usedWords = new ArrayList<>();
+        this.practiceApplication = practiceApplication;
 
-        this.addNewTranslation("sana", "word");
+        this.translations.put("sana", "word");
     }
 
     public String get(String word) {
-        return translations.get(word);
+        return this.translations.get(word);
     }
 
     public void addNewTranslation(String word, String translation) {
-        translations.put(word, translation);
+        this.translations.put(word, translation);
     }
 
-    public String drawWord() {
+    public String drawNewWord() {
+        List<String> words = new ArrayList<>(this.translations.keySet());
 
-        return new ArrayList<>(translations.keySet())
-                .get(new Random()
-                        .nextInt(translations
-                                .keySet()
-                                .size()));
+        if (usedWords.containsAll(words)) {
+            practiceApplication.finnish();
+        }
+
+        String word = words.get(new Random().nextInt(words.size()));
+        if (!usedWords.contains(word)) {
+            usedWords.add(word);
+        }
+
+        return word;
     }
 
 }
