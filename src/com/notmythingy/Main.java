61927270d9b5aa3f1559d67f83d0026ed85a707d
NotMyThingy <miska.janhunen@protonmail.com>
2020-10-27 21:25:18 +0200

    Launch app

--- src/com/notmythingy/Main.java
+++ src/com/notmythingy/Main.java
@@ -1,8 +1,11 @@
 package com.notmythingy;
 
+import javafx.application.Application;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Application.launch(PracticeApplication.class);
     }
 }
